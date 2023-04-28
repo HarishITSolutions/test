c62331c42c762967ee6021578e0051b5c43cdb2e
Harish Nerella <503356053@ge.com>
2023-04-28 08:12:39 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,12 @@
-module "policy" {
-  source              = "../../"
-  policy_manner       = "Policy"
-  policy_name         = "test"
-  policy_type         = "Custom"
-  mode                = "All"
-  policy_display_name = "test policy"
-  policy_rule = {
-    "if" : {
-      "not" : {
-        "field" : "location"
-        "in" : "[parameters('allowedLocations')]"
-      }
-    },
-    "then" : {
-      "effect" : "deny"
-    }
-  }
-  policy_parameters = {
-    "allowedLocations" : {
-      "type" : "Array",
-      "metadata" : {
-        "description" : "The list of allowed locations for resources.",
-        "displayName" : "Allowed locations",
-        "strongType" : "location"
-      }
-    }
-  }
-  metadata = {
-    "category" : "General"
-  }
-
-  policy_def_scope_type  = "subscription"
-  policy_assignment_name = "testassign"
-  subscription_id        = "/subscriptions/37ed9c9a-0d39-4823-a06c-e8121b935f89"
-  assignment_location    = "eastus"
-  assignment_parameters = {
-    "allowedLocations" : {
-      "value" : ["West Europe"]
-    }
-  }
+resource "azurerm_policy_definition" "policy" {
+  count               = var.policy_type == "Custom" && var.policy_manner == "Policy" ? 1 : 0
+  name                = var.policy_name
+  policy_type         = var.policy_type
+  mode                = var.mode
+  display_name        = var.policy_display_name
+  description         = var.policy_description
+  policy_rule         = jsonencode(var.policy_rule)
+  parameters          = jsonencode(var.policy_parameters)
+  metadata            = jsonencode(var.metadata)
+  management_group_id = var.management_group_id
 }
