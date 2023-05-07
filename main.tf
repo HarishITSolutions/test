640bcd71ef3808a050849cee97106de42024176f
Harish Nerella <503356053@ge.com>
2023-05-07 18:13:18 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -32,8 +32,13 @@ resource "azurerm_resource_group" "rg" {
 #    ]
 #  }
 #}
+
+resource "random_id" "storage_account" {
+  byte_length = 8
+}
+
 resource "azurerm_storage_account" "sa2" {
-  name                     = "vernovatestsa249"
+  name                     = "tfsta${lower(random_id.storage_account.hex)}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
