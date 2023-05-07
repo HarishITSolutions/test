64e7e30a7b90153138204edcb37afb8204df25d0
Harish Nerella <503356053@ge.com>
2023-05-07 17:53:19 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -32,13 +32,13 @@ resource "azurerm_resource_group" "rg" {
 #    ]
 #  }
 #}
-#resource "azurerm_storage_account" "sa" {
-#  name                     = "vernovatestsa"
-#  resource_group_name      = azurerm_resource_group.rg.name
-#  location                 = azurerm_resource_group.rg.location
-#  account_tier             = "Standard"
-#  account_replication_type = "GRS"
-#}
+resource "azurerm_storage_account" "sa" {
+  name                     = "vernovatestsa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
 resource "azurerm_storage_container" "sacontainer" {
   name                  = "tfstatecontainer"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
