943d3ce0a65a114b67fc4b838b259a6b9a4107ae
Harish Nerella <503356053@ge.com>
2023-05-07 18:02:50 +0000

    Updated output.tf

--- output.tf
+++ output.tf
@@ -5,7 +5,7 @@ output "resource_group_name" {
 #    value = azurerm_key_vault.kv.name
 #}
 output "storage_account_name" {
-    value = azurerm_storage_account.sa.name
+    value = azurerm_storage_account.sa2.name
 }
 #output "policy_set_definition_name" {
 #    value = azurerm_policy_set_definition.policyset.name
