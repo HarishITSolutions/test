346206262d9ba8fddfe7ad3572047381e86c44e8
Harish Nerella <503356053@ge.com>
2023-05-07 17:57:20 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ resource "azurerm_storage_account" "sa" {
 }
 resource "azurerm_storage_container" "sacontainer" {
   name                  = "tfstatecontainer"
-  resource_group_name   = "azurerm_resource_group.rg.name"
   storage_account_name  = "azurerm_storage_account.sa.name"
   container_access_type = "blob"
 }
