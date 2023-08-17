c1afe706d25f55afcca01cda3eb2b90b5ce557c6
Harish Nerella <503356053@ge.com>
2023-08-17 12:10:09 +0000

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -25,16 +25,6 @@ terraform {
 // }
 
 
-terraform {
-  backend "azurerm" {
-    subscription_id          = "37ed9c9a-0d39-4823-a06c-e8121b935f89"
-    resource_group_name      = "Testresourcegroup1"
-    storage_account_name     = "tfstatestoracc"
-    container_name           = "tfstatecontainer"
-    key                      = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
