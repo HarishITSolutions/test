68dd02fd92788d7f4a9dbf1374f560af6676e9e2
Harish Nerella <503356053@ge.com>
2023-08-22 11:29:57 +0000

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -27,10 +27,10 @@ terraform {
 
 provider "azurerm" {
   skip_provider_registration = "true"
-  subscription_id            = "a6c03ad9-19b6-44da-9bfd-60a12d0e2ea8"
-  tenant_id                  = "10af2277-49af-4f54-9c7d-709259f96c76"
-  client_id                  = "98e1f53f-0698-4bec-b960-82df79efa915"
-  client_secret              = "FWb8Q~W3HtlYAJEDPa3eF2PBjNT6e~hWARVZKc~k"
+  // subscription_id            = "a6c03ad9-19b6-44da-9bfd-60a12d0e2ea8"
+  // tenant_id                  = "10af2277-49af-4f54-9c7d-709259f96c76"
+  // client_id                  = "98e1f53f-0698-4bec-b960-82df79efa915"
+  // client_secret              = "FWb8Q~W3HtlYAJEDPa3eF2PBjNT6e~hWARVZKc~k"
   features {}
 }
 
